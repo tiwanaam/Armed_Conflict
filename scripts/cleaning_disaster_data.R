7254dacdbfd2a0e20bfc8b9f2b93240ab6424bb6
tiwanaam <amrit.tiwana@mail.utoronto.ca>
2023-09-21 11:16:18 -0400

    cleaned disaster data

---
+++ scripts/cleaning_disaster_data.R
@@ -0,0 +1,51 @@
+#-----------------------------------------------------------------
+# Author: Amrit Tiwana
+# Last updated: 2023-09-21
+# What: Read in raw data, 
+#         filter data,
+#         subset data, 
+#         create dummy variables and group data, 
+#         write new clean data
+#----------------------------------------------------------------
+
+library(here)
+here()
+
+# Read in the raw data
+
+rawdat <- read.csv(here("original", "disaster.csv"), header = TRUE)
+
+library(tidyverse)
+
+# Filter the data to include years 2000–2019 and disaster types "Earthquake" and "Drought"
+
+cleandat <- rawdat %>%
+  filter(Year >= 2000 & Year <= 2019, Disaster.Type %in% c("Earthquake", "Drought"))
+
+
+# Subset the data set to only include the following variables: Year, ISO, Disaster.type.
+
+cleandat <- cleandat %>%
+  select(Year, ISO, Disaster.Type)
+
+# Create dummy variables 'drought' and 'earthquake'
+
+cleandat <- cleandat %>%
+  mutate(drought = ifelse(Disaster.Type == "Drought", 1, 0),
+         earthquake = ifelse(Disaster.Type == "Earthquake", 1, 0))
+
+# Print the first 10 rows of the resulting data frame
+
+head(cleandat, 10)
+
+# Group the data by 'Year' and 'ISO' and calculate the sum of 'drought' and 'earthquake' for each group
+
+cleandat <- cleandat %>%
+  group_by(Year, ISO) %>%
+  summarize(drought = sum(drought), earthquake = sum(earthquake))
+
+# Print the first 10 rows of the resulting summarized data frame
+
+head(cleandat, 10)
+
+# Push to github
