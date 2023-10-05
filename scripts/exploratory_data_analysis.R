5508fb06dba2c5eb9972c8a460e3da2ca3e821ff
tiwanaam <amrit.tiwana@mail.utoronto.ca>
2023-10-05 12:39:59 -0400

    Edited EDA

--- scripts/exploratory_data_analysis.R
+++ scripts/exploratory_data_analysis.R
@@ -110,4 +110,15 @@ mean(is.na(finaldata)) # it is relatively low
 
 # Summary
 
-# There are 3,720 observations and 20 variables. There were no duplicate observations found in the data set. Approximately 18.92% of the countries in the data set experienced armed conflict. The maternal mortality rate ranges from 2.0 to 2,480.0. The infant mortality rate ranges from 1.60 to 138.10. Neonatal mortality rates ranges from 0.80 to 60.90. The under-5 mortality rate ranges from 2.00 to 224.90. This suggests that mortality rates vary significantly in the data set. On the log scales, the GDP variable looks normally distributed. Based on the scatter plots, mortality rates decrease as GDP increases. Further examining maternal mortality, the rate decreases from 2000 to 2017. The box plots examining the relationship between GDP and armed conflict overlap, indicating that countries with armed conflict and countries without armed conflict do not have different medians. The proportion of missingness is 0.00844 which is relatively low. I should look into what variables have missingingness and explore the type of missingness.
+# There are 3,720 observations and 20 variables. There were no duplicate observations 
+# found in the data set. Approximately 18.92% of the countries in the data set experienced 
+# armed conflict. The maternal mortality rate ranges from 2.0 to 2,480.0. The infant 
+# mortality rate ranges from 1.60 to 138.10. Neonatal mortality rates ranges from 0.80 to 
+# 60.90. The under-5 mortality rate ranges from 2.00 to 224.90. This suggests that mortality 
+# rates vary significantly in the data set. On the log scales, the GDP variable looks normally 
+# distributed. Based on the scatter plots, mortality rates decrease as GDP increases. Further 
+# examining maternal mortality, the rate decreases from 2000 to 2017. The box plots examining 
+# the relationship between GDP and armed conflict overlap, indicating that countries with 
+# armed conflict and countries without armed conflict do not have different medians. The 
+# proportion of missingness is 0.00844 which is relatively low. I should look into what 
+# variables have missingingness and explore the type of missingness.
