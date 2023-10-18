9e658d19f32c7ada4eaaf71cd13773b5763fe5c4
tiwanaam <amrit.tiwana@mail.utoronto.ca>
2023-10-18 16:30:09 -0400

    updated descriptive figure and baseline characteristics table

--- scripts/descriptive_figure.R
+++ scripts/descriptive_figure.R
@@ -1,24 +1,29 @@
 #-----------------------------------------------------------------
 # Author: Amrit Tiwana
 # Last updated: 2023-10-16
-# What: Create descriptive table
+# What: Create descriptive figure
 #----------------------------------------------------------------
 
 data <- read.csv(here("data", "finaldata.csv"), header = TRUE)
 
-data |>
+forfigure <- data |>
   dplyr::select(country_name, ISO, year, matmor) |>
   dplyr::filter(year < 2018) |>
-  arrange(ISO, year) |>
   group_by(ISO) |>
-  mutate(diffmatmor = matmor - matmor[1L]) 
+  mutate(diffmatmor = matmor - matmor[1L]) |>
+  arrange(ISO, desc(year)) |>
+  mutate(incmatmor = ifelse(diffmatmor[1L] > 0 , 1, 0)) |>
+  arrange(ISO, year) |>
+  ungroup() |>
+  dplyr::filter(incmatmor == 1)
+length(unique(forfigure$ISO))
 
-## Mortality trend for 2000-2017 by OECD
-
-data |>
+fig1 <- forfigure |>
   ggplot(aes(x = year, y = matmor, group = ISO)) +
-  geom_line(aes(color = as.factor(armconflict)), alpha = 0.5) +
+  geom_line(aes(color = country_name), alpha = 1, linewidth = 1) +
   xlim(c(2000,2017)) +
   scale_y_continuous(trans='log10') + 
-  labs(y = "Maternal mortality", x = "Year", color = "Armed conflict") + 
-  theme_bw()
+  labs(y = "Maternal mortality (log 10 scale)", x = "Year", color = "Country", title = "Trend in maternal mortality for countries that had an increase from 2000 to 2017") + 
+  theme_bw(base_size = 12)
+
+ggsave(fig1, file = here("figures", "fig1_increasingmaternalmortality.png"), width = 8, height = 5)
